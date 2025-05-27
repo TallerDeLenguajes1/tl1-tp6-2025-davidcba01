5bd2fd6b7876c50d91c0f944c63ec40fa75552a4
davidcba01 <davoocordoba2001@gmail.com>
2025-05-27 12:58:06 -0300

    ejercicio 1 completado

--- Program.cs
+++ Program.cs
@@ -2,13 +2,42 @@
 Console.WriteLine("Hello, World!");
 
 int a;
-
 int b;
 
-a=10;
+a = 10;
+b = a;
 
-b=a;
+Console.WriteLine("valor de a: " + a);
+Console.WriteLine("valor de b: " + b);
 
-Console.WriteLine("valor de a: "+a);
+int numero=0;
+Console.WriteLine("Ingrese un numero que quiera invertir (entero positivo):");
+string entrada = Console.ReadLine();
+bool esNumero = int.TryParse(entrada, out numero);
 
-Console.WriteLine("valor de b: "+b);
+if (esNumero)
+{
+    Console.WriteLine($"El número ingresado es: {numero}");
+
+    if (numero > 0)
+    {
+        int numeroInvertido = 0;
+
+        while (numero > 0)
+        {
+            int digito = numero % 10;
+            numeroInvertido = (numeroInvertido * 10) + digito;
+            numero /= 10;
+        }
+
+        Console.WriteLine("El numero invertido es: " + numeroInvertido);
+    } 
+    else
+    {
+        Console.WriteLine("La entrada no es un número entero válido");
+    }
+}
+else
+{
+    Console.WriteLine("La entrada no es un número entero válido");
+}
